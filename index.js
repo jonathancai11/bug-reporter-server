ff6084ba61ac48771ceca41574945201c6001266
Jonathan Cai <caimjonathan@gmail.com>
2020-10-09 21:43:34 -0700

    hello world!

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ const { nanoid } = require('nanoid');
 
 require('dotenv').config();
 
-const db = monk(process.env.MONGODB_URI);
-const urls = db.get('urls');
-urls.createIndex({ slug: 1 }, { unique: true });
-
 const app = express();
 app.enable('trust proxy');
 
@@ -22,65 +18,13 @@ app.use(morgan('common'));
 app.use(express.json());
 app.use(express.static('./public'));
 
-const notFoundPath = path.join(__dirname, 'public/404.html');
-
-app.get('/:id', async (req, res, next) => {
-  const { id: slug } = req.params;
-  try {
-    const url = await urls.findOne({ slug });
-    if (url) {
-      return res.redirect(url.url);
-    }
-    return res.status(404).sendFile(notFoundPath);
-  } catch (error) {
-    return res.status(404).sendFile(notFoundPath);
-  }
-});
-
 const schema = yup.object().shape({
   slug: yup.string().trim().matches(/^[\w\-]+$/i),
   url: yup.string().trim().url().required(),
 });
 
-app.post('/url', slowDown({
-  windowMs: 30 * 1000,
-  delayAfter: 1,
-  delayMs: 500,
-}), rateLimit({
-  windowMs: 30 * 1000,
-  max: 1,
-}), async (req, res, next) => {
-  let { slug, url } = req.body;
-  try {
-    await schema.validate({
-      slug,
-      url,
-    });
-    if (url.includes('cdg.sh')) {
-      throw new Error('Stop it. 🛑');
-    }
-    if (!slug) {
-      slug = nanoid(5);
-    } else {
-      const existing = await urls.findOne({ slug });
-      if (existing) {
-        throw new Error('Slug in use. 🍔');
-      }
-    }
-    slug = slug.toLowerCase();
-    const newUrl = {
-      url,
-      slug,
-    };
-    const created = await urls.insert(newUrl);
-    res.json(created);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.use((req, res, next) => {
-  res.status(404).sendFile(notFoundPath);
+app.get('/', async (req, res, next) => {
+  res.json("hello world!");
 });
 
 app.use((error, req, res, next) => {
@@ -95,7 +39,7 @@ app.use((error, req, res, next) => {
   });
 });
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
