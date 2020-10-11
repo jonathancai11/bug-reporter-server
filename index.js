b5e7d8459e40b56d79725cd945a27a0b8b04ef89
Jonathan Cai <caimjonathan@gmail.com>
2020-10-10 21:19:17 -0700

    use cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const monk = require('monk');
 const rateLimit = require('express-rate-limit');
 const slowDown = require('express-slow-down');
 const { nanoid } = require('nanoid');
+const cors = require('cors');	
 
 require('dotenv').config();
 
@@ -16,6 +17,7 @@ const reports = db.get('reports');
 const app = express();
 app.enable('trust proxy');
 
+app.use(cors());
 app.use(helmet());
 app.use(morgan('common'));
 app.use(express.json({limit: '10mb'})); // Set limit for size of request
